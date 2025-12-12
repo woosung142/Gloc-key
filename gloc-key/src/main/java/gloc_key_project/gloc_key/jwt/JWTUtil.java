65598ca27635dee4bbfaff058853b352ddd4ca77
anjunno <dkswnstjqsla@naver.com>
2025-12-13 01:03:14 +0900

    feat: Jwt 발급 , 검증 클래스 구현

---
+++ gloc-key/src/main/java/gloc_key_project/gloc_key/jwt/JWTUtil.java
@@ -0,0 +1,63 @@
+package gloc_key_project.gloc_key.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class JWTUtil {
+    private SecretKey secretKey;
+
+    // JWTUtil 생성자
+    public JWTUtil(@Value("${spring.jwt.secret")String secret) {
+
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    // username 확인
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("username",String.class);
+    }
+
+    // role 확인
+    public String getRole(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("role",String.class);
+    }
+
+    // 토큰 만료 확인
+    public Boolean isExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+
+
+    // 토큰 생성 메서드
+    public String creatJwt(String username, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
