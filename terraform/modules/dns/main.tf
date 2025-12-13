fe553447fef1d0f08b06f5d295cc2f6d4023c366
woosung142 <woosung5968@gmail.com>
2025-12-13 19:15:15 +0900

    fix: 내부망 DNS TTL을 60초로 수정 (스팟 인스턴스 대응)

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -23,20 +23,20 @@ resource "aws_route53_record" "argocd" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "ar.${var.domain_name}"
   type    = "A"
-  ttl     = 300
+  ttl     = 60
   records = [var.private_ip]
 }
 resource "aws_route53_record" "monitoring" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "mo.${var.domain_name}"
   type    = "A"
-  ttl     = 300
+  ttl     = 60
   records = [var.private_ip]
 }
 resource "aws_route53_record" "traefik" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "tr.${var.domain_name}"
   type    = "A"
-  ttl     = 300
+  ttl     = 60
   records = [var.private_ip]
 }
