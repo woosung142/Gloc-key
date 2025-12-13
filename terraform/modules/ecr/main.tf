b090006b24469c7f45beb909868f6edbce26e1eb
woosung142 <woosung5968@gmail.com>
2025-12-13 16:07:49 +0900

    feat: CI/CD 도입 (#9)
    * test: CI/CD 워크플로우
    
    * test: CI/CD
    
    * test: cicd
    
    * test: CI/CD
    
    * test: CI/CD 2
    
    * test: CICD 3
    
    * test: CI/CD 4
    
    * test: CI/CD 5
    
    * ci: Update backend tag to cicd-f8dd609 [skip ci]
    
    * test: CI/CD 6
    
    * ci: Update backend tag to cicd-85348d4 [skip ci]
    
    * feat: CI/CD workflow 도입
    
    * fix: ECR 최신 이미지 2개 저장 및 태그 없는 이미지 삭제
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -14,20 +14,36 @@ resource "aws_ecr_repository" "repo" { # ECR 리포지토리 생성
 resource "aws_ecr_lifecycle_policy" "policy" { # ECR 라이프사이클 정책 설정
   repository = aws_ecr_repository.repo.name
 
-  policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Keep last 2 images"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 2
+policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire untagged images immediately",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep last 2 tagged images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["latest", "cicd-", "main-"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 2
+            },
+            "action": {
+                "type": "expire"
+            }
         }
-        action = {
-          type = "expire"
-        }
-      }
     ]
-  })
+}
+EOF
 }
