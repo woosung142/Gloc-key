8c77085ed26d75f7f0c389840de2ebd0d42f837d
woosung142 <woosung5968@gmail.com>
2026-01-02 02:12:07 +0900

    feat: worker node 추가 및 인프라 수정 (#19)
    * ci: Update backend tag to main-5764f0f [skip ci]
    
    * feat: worker node 추가 및 인프라 수정
    
    * feat: worker node 추가 및 인프라 수정
    
    * fix: argocd 자동화 구성 테스트
    
    * feat: infra application 추가
    
    * feat: worker 노드 구축 및 자동화 스크립트 구성
    
    * fix: 충돌 개선
    
    * feat: sealed-secrets 추가
    
    * feat: certificate HTTPS 인증 추가
    
    * feat: certificate 추가
    
    * fix: path 경로 수정
    
    * fix: cert-manager kustomize 수정
    
    * fix: servicemonitor false 변경
    
    * feat: traefik 대시보드 추가
    
    * feat: monitoring 도입
    
    * fix: webhooks 오류 수정
    
    * feat: ingressroute grafana 추가
    
    * fix: retensionsize 단위 표기 수정
    
    * fix: prometheus, alertmanager 내부 경로 네임스페이스 오류 수정
    
    * feat: taint 마스터 노드 node-exporter 허용
    
    * feat: argocd controller cpu limit 삭제 및 tolerations effect 삭제
    
    * fix: repo-server cpu limit 삭제
    
    * fix: 하이픈 오타 수정
    
    * feat: worker node 추가 및 인프라 수정
    
    * fix: 충돌 개선
    
    * feat: worker node 추가 및 인프라 수정
    
    * fix: argocd 자동화 구성 테스트
    
    * feat: sealed-secrets 추가
    
    * feat: certificate HTTPS 인증 추가
    
    * feat: certificate 추가
    
    * fix: path 경로 수정
    
    * fix: cert-manager kustomize 수정
    
    * fix: servicemonitor false 변경
    
    * feat: traefik 대시보드 추가
    
    * feat: monitoring 도입
    
    * fix: webhooks 오류 수정
    
    * feat: ingressroute grafana 추가
    
    * fix: retensionsize 단위 표기 수정
    
    * fix: prometheus, alertmanager 내부 경로 네임스페이스 오류 수정
    
    * feat: taint 마스터 노드 node-exporter 허용
    
    * feat: argocd controller cpu limit 삭제 및 tolerations effect 삭제
    
    * fix: repo-server cpu limit 삭제
    
    * fix: 하이픈 오타 수정
    
    * fix: 수정
    
    ---------
    
    Co-authored-by: Anjunno <dkswnstjqsla@naver.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/modules/sagemaker/main.tf
+++ terraform/modules/sagemaker/main.tf
@@ -3,7 +3,7 @@ resource "aws_sagemaker_model" "this" {
   execution_role_arn = var.execution_role_arn
 
   primary_container {
-    image = var.image_uri 
+    image = var.image_uri
   }
 }
 
@@ -11,9 +11,9 @@ resource "aws_sagemaker_endpoint_configuration" "this" {
   name = "${var.project_name}-endpoint-config"
 
   production_variants {
-    variant_name          = "AllTraffic"
-    model_name            = aws_sagemaker_model.this.name # 위에서 만든 모델 연결
-  
+    variant_name = "AllTraffic"
+    model_name   = aws_sagemaker_model.this.name # 위에서 만든 모델 연결
+
     serverless_config {
       max_concurrency   = var.max_concurrency # 부모 모듈에서 받은 1
       memory_size_in_mb = var.memory_size     # 부모 모듈에서 받은 1024
