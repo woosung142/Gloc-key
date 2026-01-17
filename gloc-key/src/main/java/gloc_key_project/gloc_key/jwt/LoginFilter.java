2c552bbdaf35e072fd71ca3e0bf334f28cec1784
anjunno <dkswnstjqsla@naver.com>
2026-01-17 14:38:55 +0900

    test: 토큰 재발급 테스트를 위한 accessToken 유효기간 수정

--- gloc-key/src/main/java/gloc_key_project/gloc_key/jwt/LoginFilter.java
+++ gloc-key/src/main/java/gloc_key_project/gloc_key/jwt/LoginFilter.java
@@ -70,8 +70,8 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         String role = auth.getAuthority();
 
         // accessToken 15분
-        String accessToken = jwtUtil.creatJwt("access",userId, username, role, 15 * 60 * 1000L);
-//        String accessToken = jwtUtil.creatJwt("access",userId, username, role, 10000L);
+//        String accessToken = jwtUtil.creatJwt("access",userId, username, role, 15 * 60 * 1000L);
+        String accessToken = jwtUtil.creatJwt("access",userId, username, role, 5000L);
 
         // refreshToken 2주
         String refreshToken = jwtUtil.creatJwt("refresh", userId, username, role, 14 * 24 * 60 * 60 * 1000L);
