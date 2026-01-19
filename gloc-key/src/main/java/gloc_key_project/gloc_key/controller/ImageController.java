b2480694312ff942e42a07908a74e22a6afe6bf9
anjunno <dkswnstjqsla@naver.com>
2026-01-19 16:47:44 +0900

    test:Edge Case(원본 삭제, 중간 삭제, 자식 없음 등)를 포함한 테스트 코드 작성

--- gloc-key/src/main/java/gloc_key_project/gloc_key/controller/ImageController.java
+++ gloc-key/src/main/java/gloc_key_project/gloc_key/controller/ImageController.java
@@ -89,6 +89,8 @@ public class ImageController {
     ) {
         DeleteImageResponse response =
                 imageService.deleteImage(imageId, userDetails.getId());
+
+        return ResponseEntity.ok(response);
     }
 
 }
